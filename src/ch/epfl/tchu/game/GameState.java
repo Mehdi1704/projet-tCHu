44428c80f9a98d5d9da1e8fb30838260cc3dbe16
Ali-Mrad <aliridhamrad@MacBook-Pro-de-Ali.local>
2021-03-22 13:59:16 +0100

    ahla

--- src/ch/epfl/tchu/game/GameState.java
+++ src/ch/epfl/tchu/game/GameState.java
@@ -8,14 +8,19 @@ import java.util.Random;
 public class GameState  extends PublicGameState{
 
     private final int ticketsCount;
+    private final PublicCardState cardState;
+    private final PlayerId currentPlayerId;
+    private final Map<PlayerId, PlayerState> playerState;
+    private final PlayerId lastPlayer;
 
     private GameState(int ticketsCount, PublicCardState cardState, PlayerId currentPlayerId,
-                      Map<PlayerId, PublicPlayerState> playerState, PlayerId lastPlayer) {
+                      Map<PlayerId, PlayerState> playerState, PlayerId lastPlayer) {
         super(ticketsCount, cardState, currentPlayerId, playerState, lastPlayer);
-
-
-
-
-
+         this.ticketsCount = ticketsCount;
+         this.PublicCardState cardState = cardState;
+         this.PlayerId currentPlayerId = currentPlayerId;
+         this.Map<PlayerId, PlayerState> playerState = playerState;
+         this.PlayerId lastPlayer = lastPlayer;
+        
     }
 }
