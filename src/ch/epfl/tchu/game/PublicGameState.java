f6b0b0784fd58793b6272fc8bbe0c7d9e1c297c1
Mehdi1704 <mehdi-bouchoucha@hotmail.fr>
2021-03-22 13:58:47 +0100

    Update PublicGameState.java

--- src/ch/epfl/tchu/game/PublicGameState.java
+++ src/ch/epfl/tchu/game/PublicGameState.java
@@ -13,26 +13,23 @@ public class PublicGameState {
     private final PublicCardState cardState;
     private final PlayerId currentPlayerId;
     private final Map<PlayerId,PublicPlayerState> playerState;
-    private final PlayerId lastPlayer;
-    //TODO IllegalArgumentException si la taille de la pioche est strictement négative ou
-    // si playerState ne contient pas exactement deux paires clef/valeur,
-    // et NullPointerException si l'un des autres arguments (lastPlayer excepté!) est nul.
+    //private final PlayerId lastPlayer;
+
     public PublicGameState(int ticketsCount, PublicCardState cardState, PlayerId currentPlayerId,
                             Map<PlayerId,PublicPlayerState> playerState, PlayerId lastPlayer){
         Preconditions.checkArgument(ticketsCount >= 0 && playerState.size() == 2);
-        if (cardState==null || currentPlayerId==null || playerState==null){
+        if (cardState==null || currentPlayerId==null){
             throw new NullPointerException();
         }
         this.ticketsCount = ticketsCount;
         this.cardState = cardState;
         this.currentPlayerId = currentPlayerId;
         this.playerState = playerState;
-        this.lastPlayer = lastPlayer;
+        //this.lastPlayer = lastPlayer;
     }
 
     /**
      * Retourne la taille de la pioche de billets
-     * @return
      */
     public int ticketsCount(){
         return ticketsCount;
@@ -40,7 +37,6 @@ public class PublicGameState {
 
     /**
      * Retourne vrai si la pioche n'est pas vide
-     * @return
      */
     public boolean canDrawTickets(){
         return ticketsCount!=0;
@@ -48,7 +44,6 @@ public class PublicGameState {
 
     /**
      * Retourne la partie publique de l'état des cartes wagon/locomotive
-     * @return
      */
     public PublicCardState cardState(){
         return new PublicCardState(cardState.faceUpCards(),cardState.deckSize(), cardState.discardsSize());
@@ -57,7 +52,6 @@ public class PublicGameState {
     /**
      * retourne vrai ssi il est possible de tirer des cartes,
      * c-à-d si la pioche et la défausse contiennent entre elles au moins 5 cartes
-     * @return
      */
     public boolean canDrawCards(){
         int totalCards = cardState.deckSize() + cardState.discardsSize();
@@ -65,7 +59,6 @@ public class PublicGameState {
     }
     /**
      * Retourne l'identité du joueur actuel
-     * @return
      */
     public PlayerId currentPlayerId(){
         return currentPlayerId;
@@ -73,8 +66,6 @@ public class PublicGameState {
 
     /**
      * Retourne la partie publique de l'état du joueur d'identité donnée
-     * @param playerId
-     * @return
      */
     //TODO verifier
     public PublicPlayerState playerState(PlayerId playerId){
@@ -83,7 +74,6 @@ public class PublicGameState {
 
     /**
      * Retourne la partie publique de l'état du joueur courant
-     * @return
      */
     //TODO verifier
     public PublicPlayerState currentPlayerState(){
@@ -92,22 +82,19 @@ public class PublicGameState {
 
     /**
      * Retourne la totalité des routes dont l'un ou l'autre des joueurs s'est emparé
-     * @return
      */
-    //TODO faire
+    //TODO verifier
     public List<Route> claimedRoutes(){
-
-        return null;
+        return playerState.get(currentPlayerId()).routes();
     }
 
     /**
      * Retourne l'identité du dernier joueur,
      * ou null si elle n'est pas encore connue
      * car le dernier tour n'a pas commencé
-     * @return
      */
     public PlayerId lastPlayer(){
-        //TODO null si le dernier tour n'a pas commencé
-        return lastPlayer;
+        if (playerState.get(currentPlayerId()).carCount() <= 2) return currentPlayerId();
+        else return null;
     }
 }
