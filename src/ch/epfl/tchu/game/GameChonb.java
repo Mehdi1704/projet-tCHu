f94815655f3b4385a59f5e719d8626cb84649ce7
Mehdi1704 <mehdi-bouchoucha@hotmail.fr>
2021-04-05 17:30:37 +0200

    Update GameChonb.java

--- src/ch/epfl/tchu/game/GameChonb.java
+++ src/ch/epfl/tchu/game/GameChonb.java
@@ -1,4 +1,85 @@
 package ch.epfl.tchu.game;
 
-public class GameChonb {
+import ch.epfl.tchu.Preconditions;
+import ch.epfl.tchu.SortedBag;
+import ch.epfl.tchu.gui.Info;
+
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.Random;
+
+import static ch.epfl.tchu.game.Constants.INITIAL_TICKETS_COUNT;
+
+public final class GameChonb{
+
+    public static final int PLAYERS_COUNT = 2;
+
+
+
+    public static void play(Map<PlayerId, Player> players, Map<PlayerId,
+            String> playerNames, SortedBag<Ticket> tickets, Random rng){
+
+        Preconditions.checkArgument(players.size()==PLAYERS_COUNT && playerNames.size()==PLAYERS_COUNT);
+
+        // Initialisation
+        Map<PlayerId,Info> playerInformation = new EnumMap<>(PlayerId.class);                   //
+        playerNames.forEach((player,info) -> playerInformation.put(player, new Info(info)));    // Informations des joueurs
+        Map<PlayerId,SortedBag<Ticket>> playersTickets = new EnumMap<>(PlayerId.class);         // Tickets initiaux
+        players.forEach((key,value) -> value.initPlayers(key,playerNames));                     // Joueurs
+        GameState gameState = GameState.initial(tickets,rng);                                   // Etat de jeu
+
+        receiveInfoForBothPlayers(players,playerInformation.get(gameState.currentPlayerId()).willPlayFirst());
+
+        // Distribution des premiers tickets aux joueurs
+        for (Map.Entry<PlayerId, Player> entry : players.entrySet()){
+            entry.getValue().setInitialTicketChoice(gameState.topTickets(INITIAL_TICKETS_COUNT));
+            gameState = gameState.withoutTopTickets(INITIAL_TICKETS_COUNT);
+        }
+
+        updateStateForBothPlayers(players, gameState, gameState.currentPlayerState());
+
+        // Les joueurs gardent les tickets qu'ils choisissent
+        for (Map.Entry<PlayerId, Player> entry : players.entrySet()){
+
+        }
+
+        //p1.chooseInitialTickets();
+        //p2.chooseInitialTickets();
+
+
+
+        receiveInfoForBothPlayers(players, playerInformation.get(gameState.currentPlayerId()).keptTickets(3));
+        // receiveInfoForBothPlayers(players, playerInformation.get(gameState.()).keptTickets(3));
+
+
+
+
+
+
+    }
+
+    /**
+     *
+     * @param players
+     * @param info
+     */
+    private static void receiveInfoForBothPlayers(Map<PlayerId, Player> players, String info){
+        players.get(PlayerId.PLAYER_1).receiveInfo(info);
+        players.get(PlayerId.PLAYER_2).receiveInfo(info);
+    }
+
+    /**
+     * méthode permettant d'informer tous les joueurs d'un changement d'état,
+     * en appelant la méthode updateState de chacun d'eux.
+     * @param players
+     * @param newState
+     * @param ownState
+     */
+    private static void updateStateForBothPlayers(Map<PlayerId, Player> players,PublicGameState newState,
+                                                  PlayerState ownState){
+        players.get(PlayerId.PLAYER_1).updateState(newState,ownState);
+        players.get(PlayerId.PLAYER_2).updateState(newState,ownState);
+
+    }
+
 }
