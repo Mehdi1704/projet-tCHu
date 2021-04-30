b138d4b49f7454509211e4bea8b6d60f59a18382
Ali-Mrad <aliridhamrad@tsf-436-wpa-3-037.epfl.ch>
2021-04-30 21:06:37 +0200

    yarabi

--- src/ch/epfl/tchu/gui/ObservableGameState.java
+++ src/ch/epfl/tchu/gui/ObservableGameState.java
@@ -1,18 +1,71 @@
 package ch.epfl.tchu.gui;
 
-import ch.epfl.tchu.game.PlayerId;
-import ch.epfl.tchu.game.PlayerState;
-import ch.epfl.tchu.game.PublicGameState;
+import ch.epfl.tchu.game.*;
+import javafx.beans.property.*;
+
+import java.util.*;
+
+import static ch.epfl.tchu.game.Constants.FACE_UP_CARD_SLOTS;
 
 public class ObservableGameState {
-    //TODO constructeur et setter
+
+    private PlayerState playerState ;
+    private PublicGameState publicGameState;
+
+    //groupe des propriétés concernant l'état public de la partie
+    private final IntegerProperty poucentageTicket;
+    private final IntegerProperty pourcentageCard;
+    private final List<ObjectProperty<Card>> faceUpCards = new ArrayList<>();
+    private final Map<Route,ObjectProperty<PlayerId>>  routeObjectPropertyMap = new HashMap<>();
+
+    //groupe des propriétés concernant l'état public de chacun des joueurs
+    private final Map<PlayerId,IntegerProperty> numberOfTicketMap  = new EnumMap<>(PlayerId.class);
+    private final Map<PlayerId,IntegerProperty> numberOfCardMap = new EnumMap<>(PlayerId.class);
+    private final Map<PlayerId,IntegerProperty> numberOfWagonMap = new EnumMap<>(PlayerId.class);
+    private final Map<PlayerId,IntegerProperty> numberOfPointOfConstructionMap = new EnumMap<>(PlayerId.class);
+
+    //groupe des propriétés concernant l'état privé du joueur
+    private final List<ObjectProperty<Ticket>> listOfTicket = new ArrayList<>(); //ticket
+    private final Map<Card,IntegerProperty> numberOfEachTypeOfCardMap = new EnumMap<>(Card.class);
+    private final Map<Route, BooleanProperty> canTakeRouteMap = new HashMap<>();
+
+
     public ObservableGameState(PlayerId PlayerId){
 
-        //PublicGameState PGS = new PublicGameState(0,null,null,null,null);
-        //PlayerState PS = new PlayerState(null,null,null);
+        playerState = null ;
+        publicGameState = null ;
+
+        poucentageTicket= new SimpleIntegerProperty(0);
+        pourcentageCard = new SimpleIntegerProperty(0);
+
+        for (int slot: FACE_UP_CARD_SLOTS) {
+            faceUpCards.add(new SimpleObjectProperty<>(null));
+        }
+        // FACE_UP_CARD_SLOTS.forEach(index -> faceUpCards.add(new SimpleObjectProperty<>(null)));
+
+        Card.ALL.forEach(carte->numberOfEachTypeOfCardMap.put(carte,new SimpleIntegerProperty(0)));
+
+
+        ChMap.routes().forEach(e->{
+            routeObjectPropertyMap.put(e, new SimpleObjectProperty<>(null));
+            canTakeRouteMap.put(e,new SimpleBooleanProperty(false));
+        });
+
+
+        PlayerId.ALL.forEach(playerId -> {
+            numberOfTicketMap.put(playerId,new SimpleIntegerProperty(0));
+            numberOfCardMap.put(playerId,new SimpleIntegerProperty(0));
+            numberOfWagonMap.put(playerId,new SimpleIntegerProperty(0));
+            numberOfPointOfConstructionMap.put(playerId,new SimpleIntegerProperty(0));
+
+        });
+
+
     }
 
     public ObservableGameState setState(PublicGameState publicGameState, PlayerState playerState){
         return this;
     }
+//TODO getter , list setState.
+
 }
