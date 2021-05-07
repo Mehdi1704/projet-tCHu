2d0928178ffeda642e04d57fa9b120672f506084
Ali-Mrad <aliridhamrad@MacBook-Pro-de-Ali.local>
2021-05-07 18:22:15 +0200

    class infoview

--- src/ch/epfl/tchu/gui/InfoViewCreator.java
+++ src/ch/epfl/tchu/gui/InfoViewCreator.java
@@ -3,6 +3,7 @@ package ch.epfl.tchu.gui;
 import ch.epfl.tchu.game.PlayerId;
 import javafx.beans.binding.Bindings;
 import javafx.collections.ObservableList;
+import javafx.geometry.Orientation;
 import javafx.scene.Node;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Separator;
@@ -25,31 +26,34 @@ class InfoViewCreator {
         VBox vbox2 = new VBox();
         vbox2.setId("player-stats");
         Separator separator = new Separator();
-       // vbox2.getChildren().add(PlayerStats());
+        separator.setOrientation(Orientation.HORIZONTAL);
+
+        vbox2.getChildren().add(PlayerStats(observableGameState,PlayerId.PLAYER_1,playerNames));
+        vbox2.getChildren().add(PlayerStats(observableGameState,PlayerId.PLAYER_2,playerNames));
 
         TextFlow textFlow = new TextFlow();
         textFlow.setId("game-info");
         Bindings.bindContent(textFlow.getChildren(),textList);
 
-
-
-        //TODO liste de textes
-
+        vBox.getChildren().addAll(vbox2,separator,textFlow);
 
         return vBox;
     }
 
-    private static TextFlow PlayerStats(ObservableGameState observableGameState,PlayerId playerId){
+    private static TextFlow PlayerStats(ObservableGameState observableGameState,PlayerId playerId, Map<PlayerId, String> playerNames){
         TextFlow textFlow = new TextFlow();
-        textFlow.getStyleClass().add("Player_");
-        Circle cercle = new Circle(5);
-
-        cercle.getStyleClass().add("filled");
+        textFlow.getStyleClass().add(playerId.name());
+        Circle circle = new Circle(5);
+        circle.getStyleClass().add("filled");
 
         Text text = new Text();
-       // text.textProperty().bind(Bindings.format(StringsFr.PLAYER_STATS,));
+        text.textProperty().bind(Bindings.format(StringsFr.PLAYER_STATS,playerNames.get(playerId),
+                observableGameState.numberofTickets(playerId),
+                observableGameState.numberOfCards(playerId),
+                observableGameState.numberOfWagons(playerId),
+                observableGameState.numberOfPointsOfConstruction(playerId)));
 
-        textFlow.getChildren().addAll(text,cercle);
+        textFlow.getChildren().addAll(text,circle);
         return textFlow;
 
     }
