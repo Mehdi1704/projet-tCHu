a40985ea7293c37cf77a65663997f808d7344a47
Mehdi1704 <mehdi-bouchoucha@hotmail.fr>
2021-05-14 14:49:47 +0200

    sorm

--- test/ch/epfl/tchu/gui/testwind.java
+++ test/ch/epfl/tchu/gui/testwind.java
@@ -7,24 +7,28 @@ import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 import java.util.Map;
 
 import static ch.epfl.tchu.game.PlayerId.PLAYER_1;
 import static ch.epfl.tchu.game.PlayerId.PLAYER_2;
 
-public class testwind {
-
+public class testwind extends Application {
     public static void main(String[] args) {
+        Application.launch(args);
+    }
 
+    @Override
+    public void start(Stage primaryStage) {
 
-        Map<PlayerId, String> playerNames =
+        Map<PlayerId, String> playerNames2 =
                 Map.of(PLAYER_1, "Ada", PLAYER_2, "Charles");
-        ObservableList<Text> infos = FXCollections.observableArrayList();
+        ObservableList<Text> infos2 = FXCollections.observableArrayList();
 
-        GraphicalPlayer player = new GraphicalPlayer(PlayerId.PLAYER_1,playerNames,infos);
+        GraphicalPlayer player = new GraphicalPlayer(PlayerId.PLAYER_1, playerNames2, infos2);
 
-      //  player.chooseTickets(SortedBag.of(ChMap.tickets().subList(0,5)),(e)-> System.out.println("choisi"));
+        player.chooseTickets(SortedBag.of(ChMap.tickets().subList(0, 5)), (e) -> System.out.println("choisi"));
     }
 
 
