f3b4d9fc50d840271cb4131948f268f61debf0f2
Mehdi1704 <mehdi-bouchoucha@hotmail.fr>
2021-05-25 17:18:17 +0200

    pushili

--- src/ch/epfl/tchu/gui/ServerMain.java
+++ src/ch/epfl/tchu/gui/ServerMain.java
@@ -66,7 +66,7 @@ public class ServerMain extends Application {
             Map<PlayerId, Player> players =
                     Map.of(PLAYER_1, graphicalPlayer,
                             PLAYER_2, remotePlayerProxy);
-
+            AudioPlayer.play("/Users/mehdibouchoucha/Desktop/sons/mine.wav",true);
             new Thread(() -> Game.play(players, playerNames, SortedBag.of(ChMap.tickets()), new Random())).start();
         }
     }
